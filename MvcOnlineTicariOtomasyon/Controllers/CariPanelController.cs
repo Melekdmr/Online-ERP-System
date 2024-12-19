21c25ca4b3507899d410fc44a1633143337dc31f
Melekdmr <melekdmrr01@gmail.com>
2024-12-19 23:04:38 +0300

    Mesaj modülü eklendi. Arayüz düzenlendi.

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -30,5 +30,20 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 			var degerler = c.SatisHarekets.Where(x => x.CariId == id).ToList();
 			return View(degerler);
 		}
-    }
+		public ActionResult GelenMesajlar()
+		{
+			var mesajlar = c.Mesajlars.ToList();
+			return View(mesajlar);
+		}
+		//[HttpGet]
+		//public ActionResult YeniMesaj()
+		//{
+		//	return View();
+		//}
+		//[HttpPost]
+		//public ActionResult YeniMesaj()
+		//{
+		//	return View();
+		//}
+	}
 }
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Mesajlar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+	public class Mesajlar
+	{
+		[Key]
+		public int MesajId { get; set; }
+
+		[Column(TypeName = "Varchar")]
+		[StringLength(30)]
+		public string Gonderen { get; set; }
+
+
+		[Column(TypeName = "Varchar")]
+		[StringLength(30)]
+		public string Gonderilen { get; set; }
+
+		[Column(TypeName = "Varchar")]
+		[StringLength(30)]
+		public string Konu { get; set; }
+
+
+		[Column(TypeName = "Varchar")]
+		[StringLength(2000)]
+		public string icerik { get; set; }
+
+
+		[Column(TypeName = "Smalldatetime")]
+		public DateTime Tarih { get; set; }
+
+	}
+}
